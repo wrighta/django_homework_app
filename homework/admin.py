c013715cab13cd305b72d62af4082a89abd0f19f
wrighta999 <anne_wright27@hotmail.com>
2025-04-11 15:56:08 +0100

    small changes

--- homework/admin.py
+++ homework/admin.py
@@ -2,7 +2,5 @@ from django.contrib import admin
 from .models import HomeworkTask
 from .models import DailyHomework
 
-
 admin.site.register(DailyHomework)
 admin.site.register(HomeworkTask)
-admin.site.register()
